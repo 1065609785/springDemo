d5713840e0182a264ed840543227ec2b29e0b862
1065609785 <1065609785@qq.com>
2018-02-08 15:04:40 +0800

    springboot-swagger2

---
+++ spring-swagger2/src/main/java/com/zsy/SpringSwagger2Application.java
@@ -0,0 +1,12 @@
+package com.zsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwagger2Application.class, args);
+	}
+}
---
+++ spring-swagger2/src/main/java/com/zsy/domain/UserInfo.java
@@ -0,0 +1,50 @@
+package com.zsy.domain;
+
+public class UserInfo {
+	private long id;
+	private String userName;
+	private String password;
+	private String phone;
+	private String address;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
